0e24c42cf0933decd317e732d9588f43951292bf
Shlomi Assaf <shlomiasaf@gmail.com>
2019-01-20 18:35:53 +0200

    remove old app

--- apps/demo-app/src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-interface WebpackRequire {
-  (id: string): any;
-  (paths: string[], callback: (...modules: any[]) => void): void;
-  ensure(ids: string[], callback: (req: WebpackRequire) => void, chunkName?: string): void;
-}
-
-interface NodeRequire extends WebpackRequire {}
