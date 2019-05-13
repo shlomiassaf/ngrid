8006f897592671855721495ffe4bf2b2625ab74b
Shlomi Assaf <shlomiasaf@gmail.com>
2019-05-13 21:38:03 +0300

    misc: fix tsconfigs

--- libs/apps/ngrid/features/karma.conf.js
+++ libs/apps/ngrid/features/karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 const { join } = require('path');
-const getBaseKarmaConfig = require('../../../karma.conf');
+const getBaseKarmaConfig = require('../../../../karma.conf');
 
 module.exports = function(config) {
   const baseConfig = getBaseKarmaConfig();
@@ -10,7 +10,7 @@ module.exports = function(config) {
     ...baseConfig,
     coverageIstanbulReporter: {
       ...baseConfig.coverageIstanbulReporter,
-      dir: join(__dirname, '../../../coverage/libs/apps/ngrid/features')
+      dir: join(__dirname, '../../../../coverage/libs/apps/ngrid/features')
     }
   });
 };
