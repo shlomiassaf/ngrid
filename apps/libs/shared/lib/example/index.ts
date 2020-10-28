9b84980db17cb40fbbd1bc75f4add0237be129f2
Shlomi Assaf <shlomiasaf@gmail.com>
2020-10-28 16:39:14 +0200

    chore(demo): remove unused code

--- apps/libs/shared/lib/example/index.ts
+++ apps/libs/shared/lib/example/index.ts
@@ -12,31 +12,13 @@ export const EXAMPLE_COMPONENT_LIST: Type<any>[] = [];
 export const EXAMPLE_COMPONENTS: {[key: string]: LiveExample} = { };
 export const COMPONENTS_EXAMPLES = new Map<Type<any>, LiveExample>();
 
-const moduleGroups = new Map<string, LiveExample[]>();
-
-export function Example(selector: string, metadata: LiveExample, groupName?: string) {
-  if (groupName) {
-    moduleGroups.get(groupName).push(metadata);
-  }
-
+export function Example(selector: string, metadata: LiveExample) {
   return target => {
     metadata.component = target;
     metadata.selectorName = selector;
     EXAMPLE_COMPONENTS[selector] = metadata;
     COMPONENTS_EXAMPLES.set(target, metadata);
     EXAMPLE_COMPONENT_LIST.push(target);
-  }
-}
-
-export namespace Example {
-  export const clearGroup = (groupName: string) => moduleGroups.delete(groupName);
-  export const defineGroup = (groupName: string) => moduleGroups.set(groupName, []);
-  export const bindModule = (moduleTypeToBind: Type<any>, groupName: string) => {
-    const coll = moduleGroups.get(groupName);
-    for (const m of coll) {
-      m.moduleType = moduleTypeToBind;
-    }
-    moduleGroups.set(groupName, [])
   }
 }
 
