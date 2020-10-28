a4821e8e7ff94b9d49ad7398ffd248ea53977fc1
Shlomi Assaf <shlomiasaf@gmail.com>
2020-10-28 17:23:13 +0200

    chore(build): split gh-pages to build & publish

--- tools/scripts/build.gh-pages.sh
+++ tools/scripts/build.gh-pages.sh
@@ -4,12 +4,3 @@ npm run compile:server
 node dist/server
 
 cp dist/browser/index.html dist/browser/404.html
-
-cd dist/browser
-
-git init
-git remote add origin git@github.com:shlomiassaf/ngrid.git
-git add .
-git commit -m "update"
-git branch gh-pages
-git push --set-upstream origin gh-pages -f
