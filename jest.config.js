80a04b35e76abdb8975117757f63e9c683c77b6a
Shlomi Assaf <shlomiasaf@gmail.com>
2020-11-03 15:46:25 +0200

    chore(docs): upgrade nx

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html']
+  projects: [
+    '<rootDir>/apps/ngrid-demo-app/',
+    '<rootDir>/libs/ngrid',
+    '<rootDir>/libs/ngrid-material',
+  ],
 };
