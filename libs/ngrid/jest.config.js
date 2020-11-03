124fad573570e66fc1f7a7cc5c74919d680e1802
Shlomi Assaf <shlomiasaf@gmail.com>
2020-11-03 15:46:25 +0200

    chore(docs): upgrade nx

--- libs/ngrid/jest.config.js
+++ libs/ngrid/jest.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  name: 'ngrid',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/libs/ngrid',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  displayName: 'ngrid',
 };
