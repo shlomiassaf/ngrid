793449dd7ca8e6d58caf81f18eb877d3bafca135
Shlomi Assaf <shlomiasaf@gmail.com>
2021-05-20 23:08:20 +0300

    build: workaround build issue

--- tools/ng-packagr.transformers/ngrid-bootstrap.transformers.ts
+++ tools/ng-packagr.transformers/ngrid-bootstrap.transformers.ts
@@ -3,7 +3,7 @@ import { CopyFile } from 'ng-cli-packagr-tasks/dist/tasks/copy-file';
 import { Bump } from 'ng-cli-packagr-tasks/dist/tasks/bump';
 
 import { mergeUmdIds, updatePathsFromCache } from './base';
-import { SassBundle, SassCompile } from './tasks';
+import { SassCompile } from './tasks';
 
 const COMPILED_PATH_MAPPINGS: { [key: string]: string[] } = {};
 async function compileNgcTransformer(taskContext: EntryPointTaskContext) {
@@ -25,7 +25,6 @@ module.exports = function(ctx: NgPackagerHooksContext, registry: HookRegistry) {
     .register('compileNgc', { before: compileNgcTransformer })
     .register('writeBundles', { before: writeBundles })
     .register(CopyFile)
-    .register(SassBundle)
     .register(SassCompile)
     .register(Bump);
 }
--- tools/ng-packagr.transformers/tasks/index.ts
+++ tools/ng-packagr.transformers/tasks/index.ts
@@ -1,3 +1,4 @@
 export * from './sass';
 export * from './schematics';
 export * from './write-package-json';
+export * from './analyse-sources-workaround';
