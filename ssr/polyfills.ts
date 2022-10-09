fd0636938c38d310aa75c58a3a0bf2a60328407c
shlomiassaf <shlomi.assaf@sharegain.com>
2022-10-09 03:20:48 +0300

    chore: ssr build working

--- ssr/polyfills.ts
+++ ssr/polyfills.ts
@@ -1,9 +1,3 @@
-// working around:
-// https://github.com/angular/components/blob/461d5390d95732544db506fb6c6536f3a5803065/src/cdk-experimental/popover-edit/polyfill.ts#L40
-if (typeof global.Element === 'undefined') {
-  global.Element = class {};
-}
-
 if (typeof global.requestAnimationFrame === 'undefined') {
   global.requestAnimationFrame = (fn => {
     setTimeout(fn, 16);
